6305a5a5f9ad0bbb62bcc86e71acf79cf7df9626
Martin Matovu <matovu.synergy@gmail.com>
2019-03-04 19:08:50 +0300

    Auto-generated ES5 file from babel process

---
+++ build/script-compiled.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var simpleFn = function simpleFn() {
+  if (true) {
+    var _a = 1;
+    var b = 2;
+    console.log(_a);
+    console.log(b);
+  }
+
+  console.log(b);
+  console.log(a);
+};
