ce5714d36fcec35bfce8de4674beb0615fc81f2c
Martin Matovu <matovu.synergy@gmail.com>
2019-03-04 19:23:44 +0300

    auto-generated ES5 file by @babel

---
+++ build/play.bundle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var _learns = _interopRequireDefault(require("../js/learns6"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var array = [1, 2, 3];
+(0, _learns.default)(array, function (data) {
+  return console.log(data);
+});
