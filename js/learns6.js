6ffdddb0664c17915841f6095d5077c54b5f6122
Martin Matovu <matovu.synergy@gmail.com>
2019-03-07 15:06:02 +0300

    Added more simple high-order functions

--- js/learns6.js
+++ js/learns6.js
@@ -1,7 +1,50 @@
 const forEach = (array, fn) => {
-   let i;
-   for(i=0; i<array.length; i++)
-        fn(array[i]) 
+	// traverses an array	
+   	let i;
+	for(i=0; i<array.length; i++)
+	   fn(array[i]) 
 }
 
 export default forEach
+
+const forEachObject = (obj,fn) => {
+	// traverses object using an algorithm
+	for(var property in obj) {
+    	if(obj.hasOwnProperty(property)) {
+        	// calls the fn with key and value as its argument
+        	fn(property, obj[property])
+    	}
+	}
+}
+
+const unless = (predict,fn) => {
+	// takes a prediction, and if false, calls the fn
+    if(!predict)
+    	fn()
+}
+
+const times = (times, fn) => {
+	// takes a number and calls the passed function 
+	// as many times as the caller stated
+	for (var i = 0; i < times; i++)
+		fn(i);
+}
+
+const every = (arr, fn) => {
+	// checks if all array elements 
+	// evaluate to true by the passed function
+	let result = true;
+	for(const value of arr)
+		// ES6 'for-of' loop iterates array elements
+		// It abstracts the traversing of the array
+		result = result && fn(value)
+	return result
+}
+
+const some = (arr,fn) => {
+	let result = false;
+	for(const value of arr)
+		result = result || fn(value)
+	return result
+}
+
