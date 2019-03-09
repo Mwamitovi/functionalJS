6a9bb669d672b6c83e72d5c7f0384c18b2dda184
Martin Matovu <matovu.synergy@gmail.com>
2019-03-09 14:49:11 +0300

    Added functions - some, sortBy

--- js/learns6.js
+++ js/learns6.js
@@ -5,3 +5,22 @@ const forEach = (array, fn) => {
 }
 
 export default forEach
+
+const some = (arr,fn) => {
+	// the opposite of the every function
+	// checks if 'any' of the array elements 
+	// evaluates to true by the passed function	
+	let result = false;
+	for(const value of arr)
+		result = result || fn(value)
+	return result
+}
+
+const sortBy = (property) => {
+	// sorts an array of objects based on a property
+	return (a,b) => {
+		var result = (a[property] < b[property]) ? -1 :
+			(a[property] > b[property]) ? 1 : 0;
+		return result;
+	}
+}
