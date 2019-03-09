1b70ea78a2f02c3b1f133d8591a6bec9c32b93c9
Martin Matovu <matovu.synergy@gmail.com>
2019-03-09 14:55:29 +0300

    removed functions - forEachObject, unless, times

--- js/learns6.js
+++ js/learns6.js
@@ -7,28 +7,3 @@ const forEach = (array, fn) => {
 
 export default forEach
 
-const forEachObject = (obj,fn) => {
-	// traverses object using an algorithm
-	for(var property in obj) {
-    	if(obj.hasOwnProperty(property)) {
-        	// calls the fn with key and value as its argument
-        	fn(property, obj[property])
-    	}
-	}
-}
-
-const unless = (predict,fn) => {
-	// takes a prediction, and if false, calls the fn
-    if(!predict)
-    	fn()
-}
-
-const times = (times, fn) => {
-	// takes a number and calls the passed function 
-	// as many times as the caller stated
-	for (var i = 0; i < times; i++)
-		fn(i);
-}
-
-
-
