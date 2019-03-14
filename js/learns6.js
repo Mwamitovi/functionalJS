3a47757e0908601c64ff53fa343a5a0fe034c78b
Martin Matovu <matovu.synergy@gmail.com>
2019-03-14 12:39:53 +0300

    Added more higher-order functions - tap, unary, once and memoize

--- js/learns6.js
+++ js/learns6.js
@@ -7,3 +7,36 @@ const forEach = (array, fn) => {
 
 export default forEach
 
+
+const tap = (value) => {
+	// takes a value and returns a function having a closure over value
+    (fn) => (
+		// execute these two arguments, and return the second expression
+		typeof(fn) === 'function' && fn(value), console.log(value)
+	)
+}
+
+const unary = (fn) => {
+	// takes a function with n arguments
+	// and converts into a single argument
+	fn.length === 1 
+		? fn 
+		: (arg) => fn(arg)
+	
+}
+
+const once = (fn) => {
+	// takes an argument fn, and returns the result of it
+	// by calling it with the apply method
+	let done = false;
+	return function () {
+		return done ? undefined : ((done = true), fn.apply(this, arguments))
+	}
+}
+
+const memoize = (fn) => {
+	// for functions that take up one argument
+	// to keep remembering their outputs
+	const lookupTable = {};
+	return (arg) => lookupTable[arg] || (lookupTable[arg] = fn(arg));
+}
