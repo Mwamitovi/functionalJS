96a903328057e7ad04aee189da340ee39bf6f3e3
Martin Matovu <matovu.synergy@gmail.com>
2019-03-19 19:09:25 +0300

    Added curry() to convert function(args) into unary()

--- js/learns6.js
+++ js/learns6.js
@@ -6,3 +6,19 @@ const forEach = (array, fn) => {
 
 export default forEach
 
+let curry = (fn) => {
+	// converts a function with 'n' number of args into a nested unary()
+	if(typeof fn!=='function'){
+		 throw Error('No function provided');
+	}
+	return function curriedFn(...args){
+		if(args.length < fn.length){
+			return function(){
+				return curriedFn.apply(
+					null, args.concat( [].slice.call(arguments) )
+				);
+			};
+		}
+		return fn.apply(null, args);
+   };
+};
