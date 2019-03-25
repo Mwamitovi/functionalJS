fecc0cd154005900829e4efa2e00973a0039547e
Martin Matovu <matovu.synergy@gmail.com>
2019-03-25 13:23:23 +0300

    Added the partial(), and differentiated curry() and curryN()

--- js/learns6.js
+++ js/learns6.js
@@ -6,7 +6,16 @@ const forEach = (array, fn) => {
 
 export default forEach
 
-let curry = (fn) => {
+const curry = (binaryFn) => {
+	// converts a function with 'two' args into a nested unary()
+	return function (firstArg) {
+	  return function (secondArg) {
+		return binaryFn(firstArg, secondArg);
+	  };
+	};
+  };
+
+const curryN = (fn) => {
 	// converts a function with 'n' number of args into a nested unary()
 	if(typeof fn!=='function'){
 		 throw Error('No function provided');
@@ -24,25 +33,41 @@ let curry = (fn) => {
 };
 
 
-let match = curry(function(expr, str) {
+const match = curryN(function(expr, str) {
 	// A curried function that matches given args
 	return str.match(expr);
 });
 
-let filter = curry(function(f, ary) {
+const filter = curryN(function(f, ary) {
 	// A curried function that filters args
 	return ary.filter(f);
 });
 
 // find if args have numbers
-let hasNumber = match(/[0-9]+/)
-// find numbers in an array
-let findNumbersInArray = filter(hasNumber)
+const hasNumber = match(/[0-9]+/)
 
-let map = curry(function(f, ary) {
+// find numbers in an array
+const findNumbersInArray = filter(hasNumber)
+
+let map = curryN(function(f, ary) {
 	// A curried function that returns transformed array
 	return ary.map(f);
 });
 
 // A curried function to square all array elements
-let squareAll = map((x) => x * x)
+const squareAll = map((x) => x * x)
+
+// 
+const partial = function (fn,...partialArgs){
+	// to apply the function args partially
+	let args = partialArgs.slice(0);
+	return function(...fullArguments) {
+	  let arg = 0;
+	  for (let i = 0; i < args.length && arg < fullArguments.length; i++) {
+		if (args[i] === undefined) {
+		  args[i] = fullArguments[arg++];
+		  }
+		}
+		return fn.apply(this, args);
+	};
+  };
