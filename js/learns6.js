767de309e3b8790933b40edccafd1c784faf9639
Martin Matovu <matovu.synergy@gmail.com>
2019-04-03 14:53:41 +0300

    Added compose(), and a composite function countWords()

--- js/learns6.js
+++ js/learns6.js
@@ -6,3 +6,20 @@ const forEach = (array, fn) => {
 
 export default forEach
 
+const compose = (a,b) =>
+	// Takes output from a function, and then feeds
+	// that output as an argument to another function
+	(c) => {
+		return a(b(c));
+	}
+
+// Split words into an array, along the spaces
+let splitIntoSpaces = (str) => str.split(" ")
+
+// Returns the total no. of array elements
+let count = (array) => array.length
+
+// A composite function that counts
+// the number of words in a string
+const countWords = compose(count, splitIntoSpaces)
+
