5e599f431db356a95900f44ed8e83cb96161b3b3
Martin Matovu <matovu.synergy@gmail.com>
2019-04-03 15:06:22 +0300

    Added composeN() to handle > two functions, & composite oddOrEvenWords()

--- js/learns6.js
+++ js/learns6.js
@@ -9,6 +9,7 @@ export default forEach
 const compose = (a,b) =>
 	// Takes output from a function, and then feeds
 	// that output as an argument to another function
+	// Works with two functions only, from right to left
 	(c) => {
 		return a(b(c));
 	}
@@ -23,3 +24,15 @@ let count = (array) => array.length
 // the number of words in a string
 const countWords = compose(count, splitIntoSpaces)
 
+const composeN = (...fns) =>
+	// Mutates from the compose() and handles 'n' functions
+	// Feeding output as 'argument' to the next function
+	(value) => {
+		reduce(fns.reverse(), (acc,fn) => fn(acc), value)
+	}
+
+// Returns if argument is even(true) or odd(false)
+let oddOrEven = (ip) => ip % 2 == 0 ? "even" : "odd"
+
+// Returns if the total word count is even or odd
+const oddOrEvenWords = composeN(oddOrEven, count, splitIntoSpaces)
