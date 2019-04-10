e0709150e008971b95bc09f31b465eaec5cbb479
Martin Matovu <matovu.synergy@gmail.com>
2019-04-10 14:50:49 +0300

    Added Container(), with properties of(), and map()

--- js/learns6.js
+++ js/learns6.js
@@ -6,3 +6,19 @@ const forEach = (array, fn) => {
 
 export default forEach
 
+const Container = function(val){
+	// simple func to hold any value passed into it
+	this.value = val;
+}
+
+// utility method to abstract (saves us) writing the 'new' keyword
+// whenever we create a new Container.
+Container.of = function(value){
+	return new Container(value);
+}
+
+// method allows us to call any function 
+// on the value being held within the Container
+Container.prototype.map = function(fn){
+	return Container.of(fn(this.value));
+}
