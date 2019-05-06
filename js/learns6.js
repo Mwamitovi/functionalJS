a1e1dd965018e91d31bbc5d1b10e5ef7c6460703
Martin Matovu <matovu.synergy@gmail.com>
2019-05-06 10:33:03 +0300

    Added Either() functor + parts - Some(), Nothing(), & real-world example

--- js/learns6.js
+++ js/learns6.js
@@ -15,13 +15,13 @@ import {arrayUtils} from '..js/arrayES6'
 const Container = function(val){
 	// simple function to hold any value passed into it
 	this.value = val;
-}
+};
 
 // utility method to abstract (saves us) writing the 'new' keyword
 // whenever we create a new Container.
 Container.of = function(value){
 	return new Container(value);
-}
+};
 
 // method allows us to call any function 
 // on the value being held within the Container
@@ -37,12 +37,12 @@ Container.prototype.map = function(fn){
 // declaring the functor object
 const MayBe = function(val) {
 	this.value = val;
-}
+};
 
 // of() method, a pointer functor, returns instance of object
 MayBe.of = function(val) {
 	return new MayBe(val);
-}
+};
 
 // isNothing() method, returns boolean true/false
 MayBe.prototype.isNothing = function() {
@@ -71,7 +71,7 @@ let getTopPosts = (type) => {
 		response = { message: "Something went wrong", errorCode: err['statusCode']}
 	}
 	return response;
-}
+};
 
 // Using MayBe() to get the Top 10 posts
 let getTopTenPosts = (type) => {
@@ -88,7 +88,82 @@ let getTopTenPosts = (type) => {
 					}
 				 ))
 				 
-}
+};
 
 // We can call our function with a valid daily monitor name like "latest"
 getTopTenPosts('latest')
+
+
+/**
+ * @see Either(), a supertype of Some() and Nothing()
+ * A functor that helps us preserve our "error message" while branching out
+ */
+// defining Either()
+const Either = {
+	Some: Some,
+	Nothing: Nothing
+};
+
+// declaring the parts of the Either() functor object
+
+// Some()
+const Some = function(val) {
+	this.value = val;
+};
+
+Some.of = function(val) {
+	return new Some(val);
+};
+
+Some.prototype.map = function(fn){
+	return Some.of(fn(this.value));
+};
+
+// Nothing()
+const Nothing = function(val) {
+	this.value = val;
+};
+
+Nothing.of = function(val) {
+	return new Nothing(val);
+};
+
+Some.prototype.map = function(f){
+	return this;
+};
+
+/**
+ * Real world application of Either()
+ * 
+ * The API example, using Either(), to get the "top", "latest", "hot" news.
+ */
+let getTopPostsEither = (type) => {
+	let response;
+	try {
+		response = Some.of(JSON.parse(
+			request('GET',"https://www.dailymonitor.co.ug/news/" + type + ".json?limit=10").getBody('utf8')
+		))
+	} catch(err) {
+		response = Nothing.of(
+			{ message: "Something went wrong", errorCode: err['statusCode'] }
+		)
+	}
+	return response;
+}
+
+let getTopTenPostsEither = (type) => {
+	let response = getTopPostsEither(type);
+	return response.map((arr) => arr['data'])
+				   .map((arr) => arr['children'])	
+				   .map((arr) => arrayUtils.map(arr,
+						(x) => {
+							return {
+								title: x['data'].title,
+								url: x['data'].url
+							}
+						}
+					))	
+}
+
+// Calling the Either() functor
+getTopTenPostsEither('hot')
