cbc400dd55d922a4d21a428a190a46a1512b03f2
Martin Matovu <matovu.synergy@gmail.com>
2019-05-24 14:31:16 +0300

    Added forEach(), example usage

--- playground/play.js
+++ playground/play.js
@@ -1,5 +1,9 @@
 import forEach from '../js/learns6'
 
 var array = [1,2,3]
+//print the console
+forEach(array,(data) => console.log(data))
+//double the contents
+forEach(array,(data) => console.log(2 * data))
 
-forEach(array, (data) => console.log(data))
+
