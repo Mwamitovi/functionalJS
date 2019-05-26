67972f47baa591dfe1bec05ed8073bb6e63e6d34
Martin Matovu <matovu.synergy@gmail.com>
2019-05-26 09:11:30 +0300

    Added functions curry(), curryN() and partial()

--- js/learns6.js
+++ js/learns6.js
@@ -164,4 +164,64 @@ const arrayUtils = {
 
 }
 
-export {forEach, forEachObject, unless, times, every, some, sortBy, tap, unary, once, memorize, arrayUtils }
+
+const curry = (binaryFn) => {
+	// converts a function with 'two' args into a nested unary()
+	return function (firstArg) {
+	  	return function (secondArg) {
+			return binaryFn(firstArg, secondArg);
+	  	};
+	};
+};
+
+
+const curryN = (fn) => {
+	// converts a function with 'n' number of args into a nested unary()
+	if(typeof fn !== 'function'){
+		throw Error('No function provided');
+	}
+	return function curriedFn(...args){
+		if(args.length < fn.length){
+			return function(){
+				return curriedFn.apply(
+					null, args.concat( [].slice.call(arguments) )
+				);
+			};
+		}
+		return fn.apply(null, args);
+   	};
+};
+
+
+const partial = function (fn,...partialArgs){
+	// apply the function args partially
+	let args = partialArgs.slice(0);
+	return function(...fullArguments) {
+	  	let arg = 0;
+	  	for (let i = 0; i < args.length && arg < fullArguments.length; i++) {
+			if (args[i] === undefined) {
+		  		args[i] = fullArguments[arg++];
+		  	}
+		}
+		return fn.apply(this, args);
+	};
+};
+
+
+export {
+	forEach, 
+	forEachObject, 
+	unless, 
+	times, 
+	every, 
+	some, 
+	sortBy, 
+	tap, 
+	unary, 
+	once, 
+	memorize, 
+	arrayUtils,
+	curry,
+	curryN,
+	partial
+}
