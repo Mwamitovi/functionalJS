b40cd1e513b072375e54b0abf02f2646138527e0
Martin Matovu <matovu.synergy@gmail.com>
2019-05-26 10:05:41 +0300

    Added functors -> Container, MayBe, Some, Nothing, and Either

--- js/learns6.js
+++ js/learns6.js
@@ -223,6 +223,88 @@ const pipe = (...fns) =>
 		reduce(fns, (acc,fn) => fn(acc), value);
 	}
 
+/**
+ * @see Functor
+ * Is a plain object/class that implements the map() function while running
+ * over each value in the object to produce a new object.
+ */
+const Container = function(val){
+	// simple function to hold any value passed into it
+	this.value = val;
+};
+
+// utility method to abstract (saves us) writing the 'new' keyword
+// whenever we create a new Container.
+Container.of = function(value){
+	return new Container(value);
+};
+
+// method allows us to call any function 
+// on the value being held within the Container
+Container.prototype.map = function(fn){
+	return Container.of(fn(this.value));
+};
+
+/**
+ * @see MayBe()
+ * A functor that allows us to handle errors in our code in a more functional way.
+ */
+// declaring the functor object
+const MayBe = function(val) {
+	this.value = val;
+};
+
+// of() method, a pointer functor, returns instance of object
+MayBe.of = function(val) {
+	return new MayBe(val);
+};
+
+// isNothing() method, returns boolean true/false
+MayBe.prototype.isNothing = function() {
+	return (this.value === null || this.value === undefined);
+};
+
+// map() method, returns 
+MayBe.prototype.map = function(fn) {
+	return this.isNothing() ? MayBe.of(null) : MayBe.of(fn(this.value));
+};
+
+// Nothing()
+const Nothing = function(val) {
+	this.value = val;
+};
+
+Nothing.of = function(val) {
+	return new Nothing(val);
+};
+
+Nothin.prototype.map = function(f){
+	return this;
+};
+
+// Some()
+const Some = function(val) {
+	this.value = val;
+};
+
+Some.of = function(val) {
+	return new Some(val);
+};
+
+Some.prototype.map = function(fn){
+	return Some.of(fn(this.value));
+};
+
+/**
+ * @see Either(), a supertype of Some() and Nothing()
+ * A functor that helps us preserve our "error message" while branching out
+ */
+// defining Either()
+const Either = {
+	Some: Some,
+	Nothing: Nothing
+};
+
 
 export {
 	forEach, 
@@ -241,5 +323,8 @@ export {
 	curryN,
 	partial,
 	composeN,
-	pipe
+	pipe,
+	some,
+	Nothing,
+	Either
 }
